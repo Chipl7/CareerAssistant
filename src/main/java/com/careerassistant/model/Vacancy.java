0f7fda9313aee2d00dd022f3866692b1b391aaea
Artem <artem.chipl@gmail.com>
2025-03-13 00:29:41 +0700

    add model(Vacancy)

---
+++ src/main/java/com/careerassistant/model/Vacancy.java
@@ -0,0 +1,50 @@
+package com.careerassistant.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table (name = "vacancy")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vacancy {
+
+    @Id
+    @SequenceGenerator(name = "vacancy_id_seq", sequenceName = "vacancy_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "vacancy_id_seq")
+    private Long id;
+
+    private String name;
+
+    private String alternateUrl;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAlternateUrl(String alternateUrl) {
+        this.alternateUrl = alternateUrl;
+    }
+
+    public String getAlternateUrl() {
+        return alternateUrl;
+    }
+}
