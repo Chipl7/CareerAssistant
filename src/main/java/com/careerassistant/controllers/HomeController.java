85ad28f282dfb2616d9d6fd63a75125c2abe8553
Artem <artem.chipl@gmail.com>
2025-03-13 00:30:08 +0700

    add HomeController

--- src/main/java/com/careerassistant/controllers/HomeController.java
+++ src/main/java/com/careerassistant/controllers/HomeController.java
@@ -1,13 +1,27 @@
 package com.careerassistant.controllers;
 
+import com.careerassistant.model.Vacancy;
+import com.careerassistant.service.VacancyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    private VacancyService vacancyService;
+
+    @Autowired
+    public HomeController(VacancyService vacancyService) {
+        this.vacancyService = vacancyService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        vacancyService.findAndSaveVacancy();
+//        model.addAttribute("vacancyList", vacancyService.findAll());
+//        model.addAttribute("vacancy", new Vacancy());
         return "home";
     }
 }
