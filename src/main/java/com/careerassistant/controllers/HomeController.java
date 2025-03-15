d449abf75b905e8f82e8d43a1f111a2fe95faac8
Artem <artem.chipl@gmail.com>
2025-03-15 22:51:28 +0700

    change HomeController

--- src/main/java/com/careerassistant/controllers/HomeController.java
+++ src/main/java/com/careerassistant/controllers/HomeController.java
@@ -2,14 +2,19 @@ package com.careerassistant.controllers;
 
 import com.careerassistant.model.Vacancy;
 import com.careerassistant.service.VacancyService;
+import org.apache.tomcat.Jar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-
     private VacancyService vacancyService;
 
     @Autowired
@@ -18,10 +23,15 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model) {
-        vacancyService.findAndSaveVacancy();
-//        model.addAttribute("vacancyList", vacancyService.findAll());
-//        model.addAttribute("vacancy", new Vacancy());
+    public String home(@RequestParam(value = "vacancyName", required = false) String vacancyName, Model model) {
+        model.addAttribute("vacancyName", new String());
+        model.addAttribute("vacancyList", vacancyService.findAll());
         return "home";
+    }
+
+    @PostMapping("/")
+    public String vacancy(@RequestParam("vacancyName") String vacancyName, Model model) {
+        vacancyService.findAndSaveVacancy(vacancyName);
+        return "redirect:/";
     }
 }
