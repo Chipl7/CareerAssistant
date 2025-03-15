b15dad73586872c7dfe25548694bf73fbb7995a3
Artem <artem.chipl@gmail.com>
2025-03-15 22:51:52 +0700

    add method in VacancyRepository

--- src/main/java/com/careerassistant/repository/VacancyRepository.java
+++ src/main/java/com/careerassistant/repository/VacancyRepository.java
@@ -4,5 +4,8 @@ package com.careerassistant.repository;
 import com.careerassistant.model.Vacancy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
+    List<Vacancy> findByNameContaining(String vacancyName);
 }
