78e794f194424ad30605a765db3d351e1476ac29
Artem <artem.chipl@gmail.com>
2025-03-16 23:30:52 +0700

    change schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS vacancy
 (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(200) not null,
-    url VARCHAR(200) not null
+    keyword VARCHAR(255),
+    name VARCHAR(255) not null,
+    url VARCHAR(2048)
+--     experience VARCHAR(255) not null,
+--     salary BIGSERIAL,
+--     city_name VARCHAR(255),
+--     street VARCHAR(255),
+--     work_format VARCHAR(255),
+--     professional_role VARCHAR(255)
 );
-CREATE SEQUENCE IF NOT EXISTS vacancy_id_seq START WITH 1 INCREMENT BY 1;
+-- CREATE SEQUENCE IF NOT EXISTS vacancy_id_seq START WITH 1 INCREMENT BY 1;
