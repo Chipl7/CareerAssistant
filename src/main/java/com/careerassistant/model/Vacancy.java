5ea11b0dfa15969fe884c7ec8aa97d2dfe1d9189
Artem <artem.chipl@gmail.com>
2025-03-16 23:30:38 +0700

    change Vacancy

--- src/main/java/com/careerassistant/model/Vacancy.java
+++ src/main/java/com/careerassistant/model/Vacancy.java
@@ -1,5 +1,6 @@
 package com.careerassistant.model;
 
+import com.careerassistant.service.dto.HhVacancyDesc;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -16,39 +17,40 @@ import lombok.Setter;
 public class Vacancy {
 
     @Id
-    @SequenceGenerator(name = "vacancy_id_seq", sequenceName = "vacancy_id_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "vacancy_id_seq")
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
     @Column(name = "url")
-    @JsonProperty("alternate_url")
     private String alternateUrl;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//    private String experience;
+//
+////    private Long salary;
+//
+//    @Column(name = "city_name")
+//    private String cityName;
+//
+//    private String street;
+//
+//    @Column(name = "work_format")
+//    private String workFormat;
+//
+//    @Column(name = "professional_role")
+//    private String professionalRole;
 
-    public Long getId() {
-        return id;
-    }
+    private String keyword;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-    public void setAlternateUrl(String alternateUrl) {
-        this.alternateUrl = alternateUrl;
-    }
-
-    public String getAlternateUrl() {
-        return alternateUrl;
+    public Vacancy(HhVacancyDesc desc, String keyword) {
+        this.id = desc.getId();
+        this.name = desc.getName();
+        this.alternateUrl = desc.getAlternateUrl();
+//        this.experience = desc.getExperience().getName();
+//        this.salary = (desc.getSalary().getFrom() != null) ? desc.getSalary().getFrom() : null;
+//        this.cityName = desc.getAddress().getCity();
+//        this.street = desc.getAddress().getStreet();
+//        this.workFormat = desc.getWorkFormat().getName();
+//        this.professionalRole = desc.getProfessionalRoles().getName();
+        this.keyword = keyword;
     }
 }
