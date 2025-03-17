1440eb2db8dcfd4f4e0bed4fdde5761ba281786d
Artem <artem.chipl@gmail.com>
2025-03-17 23:29:07 +0700

    update VacancyRepo

--- src/main/java/com/careerassistant/repository/VacancyRepository.java
+++ src/main/java/com/careerassistant/repository/VacancyRepository.java
@@ -2,10 +2,15 @@ package com.careerassistant.repository;
 
 
 import com.careerassistant.model.Vacancy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface VacancyRepository extends JpaRepository<Vacancy, Long> {
-    List<Vacancy> findByKeyword(String keyword);
+//    List<Vacancy> findByKeyword(String keyword);
+    Page<Vacancy> findByKeyword(String keyword, Pageable pageable);
+    Long countByKeyword(String keyword);
+    Page<Vacancy> findAll(Pageable pageable);
 }
